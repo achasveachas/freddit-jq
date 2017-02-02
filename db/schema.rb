cc0cb983ba714fe88464663fd4f651b02801f2e6
Yechiel K <chilikkal@gmail.com>
2017-02-01 21:15:31 -0500

    created conversations model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202014850) do
+ActiveRecord::Schema.define(version: 20170202021110) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.string   "topic"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
