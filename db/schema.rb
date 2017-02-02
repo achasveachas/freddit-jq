eeee2f487f1508b93e7efd74eaef77d64c5096e8
Yechiel K <chilikkal@gmail.com>
2017-02-01 21:29:56 -0500

    added user column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202022451) do
+ActiveRecord::Schema.define(version: 20170202022926) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 20170202022451) do
     t.string   "commentable_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
     t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "conversations", force: :cascade do |t|
