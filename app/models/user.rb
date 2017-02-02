9c24976bff99838ff2a1d7a12a16de189290688f
Yechiel K <chilikkal@gmail.com>
2017-02-01 20:35:34 -0500

    added secure password and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, :email, presence: true
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
+  validates :passord, length: {minimum: 6}, on: :create
+  validates :passord, confirmation: true, on: :create
 end
