3d180db66aa40be57a23718ad1f6a0d026d8a0c1
Yechiel K <chilikkal@gmail.com>
2017-02-01 20:50:07 -0500

    added default false value to admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202012924) do
+ActiveRecord::Schema.define(version: 20170202014850) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
