246b70dc2359b32a9349da6f00cac588ea176bc9
Yechiel K <chilikkal@gmail.com>
2017-02-06 00:24:40 -0500

    added admin button to show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def ban_button
+    @user.banned ? "Unban User" : "Ban User"
+  end
+
+  def admin_button
+    @user.admin ?  "Revoke Admin Privilages" : "Make Admin"
+  end
 end
