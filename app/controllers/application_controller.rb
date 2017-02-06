831d97992469d1cd7f1b07808b60639f79508860
Yechiel K <chilikkal@gmail.com>
2017-02-05 21:27:08 -0500

    added helper methods for login, logout, current_user and can_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::Base
   def index
     render 'index'
   end
+
+  private
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session[:user_id] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) unless session[:user_id] == nil
+  end
+
+  def can_edit?
+    current_user && (current_user.admin || @user == current_user)
+  end
 end
