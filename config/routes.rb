549969663abf63f0a99bff01efc3035a4ba2523d
Yechiel K <chilikkal@gmail.com>
2017-02-06 00:12:38 -0500

    added banning and admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
 
+  patch 'user/:id/ban' => 'users#ban', as: 'user_ban'
+  patch 'user/:id/admin' => 'users#admin', as: 'user_admin'
+
   resources :users
 end
