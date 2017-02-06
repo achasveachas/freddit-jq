310f9b8ab8f07a36810ca480ac292f021bd8df8d
Yechiel K <chilikkal@gmail.com>
2017-02-06 01:06:27 -0500

    added user edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       login(@user)
       redirect_to @user
     else
-      render :new
+      render 'users/new'
     end
   end
 
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :image_url)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :image_url, :age, :location)
   end
 end
