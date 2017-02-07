5e996e4c14c28c5e75e0c0f28750dabfad78d333
Yechiel K <chilikkal@gmail.com>
2017-02-06 23:58:11 -0500

    added routes for oauth authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  #Omniauth routes
+  get '/auth/github/callback' => 'sessions#create_oauth'
+  get '/auth/failure' => 'index#home'
 
   patch 'user/:id/ban' => 'users#ban', as: 'user_ban'
   patch 'user/:id/admin' => 'users#admin', as: 'user_admin'
