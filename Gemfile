dc9fb3ad922ff7d0773b49b14476eb26caceee57
Yechiel K <chilikkal@gmail.com>
2017-02-07 00:50:44 -0500

    added create from oauth action

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'omniauth'
 gem 'omniauth-github'
 gem 'faker', '~> 1.7', '>= 1.7.2'
 
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,17 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_oauth
+  if @user = User.find_by_username(request.env['omniauth.auth']['info']['username'])
+    login(@user)
+    redirect_to @user
+  else
+    @user = User.create(email: request.env['omniauth.auth']['info']['email'], username: request.env['omniauth.auth']['info']['username'], password: "randompassword", password_confirmation: "randompassword")
+    login(@user)
+    redirect_to edit_user_path(@user)
+  end
+end
+
   def destroy
     log_out
     redirect_to root_path
