145d4a401ebaecf5d021bd3563150db37a7175c5
Yechiel K <chilikkal@gmail.com>
2017-02-10 12:08:33 -0500

    created set_ancestor method for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,26 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  belongs_to :conversation, as: :ancestor
+  belongs_to :conversation, foreign_key: :ancestor_id
   has_many :comments, as: :commentable
 
-  after_create :update_parent_sort_column, :set_ancestor
+  after_create :update_ancestor_sort_column, :set_ancestor
 
 
 
   private
 
-  def update_parent_sort_column
+  def set_ancestor
+    current_child = self
+
+    until current_child.commentable_type == "Conversation"
+      current_child = current_child.commentable
+    end
+
+    self.ancestor_id = current_child.commentable_id
+  end
+
+  def update_ancestor_sort_column
 
   end
 
