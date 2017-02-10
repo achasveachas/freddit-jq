ff3b90cce707d2066cc79c25aa92ac3c244e2a4b
Yechiel K <chilikkal@gmail.com>
2017-02-10 11:29:52 -0500

    added method to initialize sort_column at creation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,11 +2,13 @@ class Conversation < ApplicationRecord
   belongs_to :user
   validates :topic, presence: true
   has_many :comments, as: :commentable, :dependent => :destroy
+  before_create :init_sort_column
 
-  def self.sort_by_last_reply
-    sorted_array = self.all.sort_by do |convo|
-      convo.comments.last.created_at
-    end
-    sorted_array.reverse
+  private
+
+  def init_sort_column
+    self.sort_column = self.created_at || Time.now
   end
+
+
 end
