cbe3393a4ad13abf49354c744ba1f22b08919e5b
Yechiel K <chilikkal@gmail.com>
2017-02-12 13:46:46 -0500

    got the show-more link to display the whole comment via AJAX call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $('.js-readMore').click(function (event) {
+    event.preventDefault()
+    showComment(event)
+  })
+})
