41e41bc779ed1f7d180327e011d90637105a74a0
Yechiel K <chilikkal@gmail.com>
2017-02-12 15:27:37 -0500

    updated the comment partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'comments/index'
-
-  get 'comments/show'
-
-  get 'comments/new'
-
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'conversations#index'
 
@@ -30,6 +16,10 @@ Rails.application.routes.draw do
   resources :users
 
   resources :conversations do
+    resources :comments
+  end
+
+  resources :comments do
     resources :comments
   end
 end
