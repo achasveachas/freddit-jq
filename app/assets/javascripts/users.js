22b6c35733dc9a86c65827af9b0fe6cc5e5d8c91
Yechiel K <chilikkal@gmail.com>
2017-02-14 20:56:31 -0500

    created a User JS  constructor

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+function User(id, username, email, image_url, admin, banned, age, location) {
+  this.id = id
+  this.username = username
+  this.email = email
+  this.image = image_url
+  this.admin = admin
+  this.banned = banned
+  this.age = age
+  this.location = location
+}
