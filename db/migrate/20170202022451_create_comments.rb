471a237974604b5da91422c308f988093a4d7f78
Yechiel K <chilikkal@gmail.com>
2017-02-22 22:20:22 -0500

    fixed comments migration to support postgresql

--- db/migrate/20170202022451_create_comments.rb
+++ db/migrate/20170202022451_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :body
-      t.belongs_to :commentable, foreign_key: true
+      t.integer :commentable_id
       t.string :commentable_type
 
       t.timestamps
