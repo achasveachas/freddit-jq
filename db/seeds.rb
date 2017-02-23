96569507d6822c25879fceffac911e78693a051c
Yechiel K <chilikkal@gmail.com>
2017-02-22 22:45:34 -0500

    fixed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  User.create(username: Faker::Internet.user_name, email: Faker::Internet.free_email, password: "testtest", password_confirmation: "testtest", image_url: Faker::LoremPixel.image)
-end
+# 10.times do
+#   User.create(username: Faker::Internet.user_name, email: Faker::Internet.free_email, password: "testtest", password_confirmation: "testtest", image_url: Faker::LoremPixel.image)
+# end
 
 30.times do
   c = Conversation.new(topic: Faker::Hipster.sentence)
