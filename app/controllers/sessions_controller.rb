b4e1d50061355163f47f97b0fe2422f9c7145788
Yechiel K <chilikkal@gmail.com>
2017-02-24 15:04:53 -0500

    fixed the oauth  action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,21 +15,21 @@ class SessionsController < ApplicationController
 
   def create_oauth
 
-  if @user = User.find_by_username(request.env['omniauth.auth']['info']['nickname'])
-    login(@user)
-    redirect_to root_path
-  else
-    password = Faker::Internet.password(18)
-    @user = User.create(email: request.env['omniauth.auth']['info']['email'], username: request.env['omniauth.auth']['info']['nickname'], password: password, password_confirmation: password, image_url: request.env['omniauth.auth']['info']['image'])
-    login(@user)
-    redirect_to edit_user_path(@user)
+    if @user = User.find_by_username(request.env['omniauth.auth']['info']['nickname'])
+      login(@user)
+      redirect_to root_path
+    else
+      password = Faker::Internet.password(18)
+      @user = User.create(email: request.env['omniauth.auth']['info']['email'], username: request.env['omniauth.auth']['info']['nickname'], password: password, password_confirmation: password, image_url: request.env['omniauth.auth']['info']['image'])
+      login(@user)
+      redirect_to edit_user_path(@user)
+    end
   end
 
   def failed_oauth
     flash[:error] = "Github Authentication Failed"
     redirect_to signin_path
   end
-end
 
   def destroy
     log_out
